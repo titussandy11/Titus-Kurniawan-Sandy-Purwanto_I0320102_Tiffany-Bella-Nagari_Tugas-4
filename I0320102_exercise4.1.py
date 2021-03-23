8fa425ff702c5b9785a45ea8c626567d701e5ad9
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-23 23:32:26 +0700

    Exercise 4.1

--- I0320102_exercise4.1.py
+++ I0320102_exercise4.1.py
@@ -1,1 +1,27 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 4.1
 
+#Operator Aritmetika
+
+x = 15
+y = 4
+
+#Penjumlahan
+print('x + y = ', x+y)
+
+#Pengurangan
+print('x - y = ', x-y)
+
+#Perkalian
+print('x * y = ', x * y)
+
+#Pembagian
+print('x / y = ', x/y)
+
+#Pembagian dengan pembulatan ke bawah
+print('x // y = ', x//y)
+
+#Pangkat
+print('x ** y = ', x**y)
