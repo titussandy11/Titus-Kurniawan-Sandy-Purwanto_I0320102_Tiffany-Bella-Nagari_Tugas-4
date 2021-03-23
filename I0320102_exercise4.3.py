11fcef20c14ffaefddc255a2f5b19bfc87b87ae2
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-23 23:46:53 +0700

    Exercise 4.3

--- I0320102_exercise4.3.py
+++ I0320102_exercise4.3.py
@@ -1,1 +1,17 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 4.3
 
+#Operator Logika
+x = True
+y = False
+
+#and
+print('x and y is', x and y)
+
+#or
+print('x or y is', x or y)
+
+#not
+print('not x is', not x)
