af9f48ca418b4be3b2f50feef32bc19cace6ffae
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-23 23:41:10 +0700

    Exercise 4.2

--- I0320102_exercise4.2.py
+++ I0320102_exercise4.2.py
@@ -1,1 +1,26 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 4.2
 
+x = 10
+y = 12
+z = 15
+
+#Lebih besar dari
+print('x > y = ', x > y)
+
+#Lebih kecil dari
+print('x < y = ', x < y)
+
+#Persamaan
+print('x == y = ', x == y)
+
+#Pertidaksamaan
+print('x != y = ', x != y)
+
+#Lebih besar atau sama dengan
+print('x >= y = ', x >= y)
+
+#Lebih kecil atau sama dengan
+print('x <= y = ', x <= y)
