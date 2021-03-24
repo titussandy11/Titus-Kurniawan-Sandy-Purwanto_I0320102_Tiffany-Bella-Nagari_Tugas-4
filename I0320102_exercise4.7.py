fcb10f5daffe97370800224126619a1e77aec999
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-24 16:04:19 +0700

    Exercise 4.7

--- I0320102_exercise4.7.py
+++ I0320102_exercise4.7.py
@@ -1,1 +1,14 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 4.7
 
+#string
+needle = "ha"
+haystack = "hello world"
+
+#check
+if needle in haystack :
+    print(needle, "is present the string", haystack)
+else :
+    print("Not found")
