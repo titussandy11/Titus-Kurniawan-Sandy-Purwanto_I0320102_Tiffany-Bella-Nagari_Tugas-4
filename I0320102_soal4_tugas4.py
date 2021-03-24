bed0000bdf4424908d0cd23785e2a9030d77b662
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-24 22:47:52 +0700

    Soal 4

--- I0320102_soal4_tugas4.py
+++ I0320102_soal4_tugas4.py
@@ -1,1 +1,8 @@
-
+print("Kursus Online 'Gajah Operation'")
+masukkan_usia_siswa = int(input('Masukkan usia anda :'))
+print(masukkan_usia_siswa >= 21)
+ujian_kualifikasi = input("Apakah anda lulus ujian kualifikasi (lulus/tidak ?")
+if masukkan_usia_siswa >= 21 and ujian_kualifikasi == "lulus":
+    print("Anda dapat mendaftar di kursus")
+else:
+    print("Maaf, anda tidak dapat mendaftar di kursus")
