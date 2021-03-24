b3d5f2661dcd11f5f7b64cf94dd75e553cfe326c
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-24 19:43:24 +0700

    Exercise 4.10

--- I0320102_exercise4.10.py
+++ I0320102_exercise4.10.py
@@ -1,1 +1,15 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 4.10
 
+string : Hello World
+
+skipping : x x x x x
+final str : HloWrd
+
+#string
+str = "Hello World"
+
+#Skip
+new_str = str[0:2]
