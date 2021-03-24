2dcf479a73f0f4a91ea965cb74063bbe03ce4db2
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-24 19:48:52 +0700

    Exercise 4.12

--- I0320102_exercise4.12.py
+++ I0320102_exercise4.12.py
@@ -1,1 +1,18 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 4.12
 
+a = 60
+b = 13
+c = 0
+
+c = a & b
+
+print("Line 1-Value of c is ", c)
+
+c = a | b;
+print("Line 2-Value of c is ", c)
+
+c = a^b;
+print("Line 3-Value of c is ", c)
