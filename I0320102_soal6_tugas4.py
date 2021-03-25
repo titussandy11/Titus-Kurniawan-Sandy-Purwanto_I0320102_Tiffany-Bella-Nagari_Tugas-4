fddf02d280d45f700ed8bf3b415859c84bd791fb
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-25 08:49:58 +0700

    Soal 6

--- I0320102_soal6_tugas4.py
+++ I0320102_soal6_tugas4.py
@@ -1,4 +1,31 @@
-a = 4
-b = 11
+x = 4
+y = 11
 
-c = 4 ^ 11
+z = 4 | 11
+print("nilai:" ,x,"binary" ,format(x, '03b'))
+print("nilai:" ,y, "binary",format(y, '04b'))
+print("nilai:" ,z, "binary",format(z, '07b'))
+
+print("======================")
+z = 4 >> 11
+print("nilai:" ,x,"binary" ,format(x, '03b'))
+print("nilai:" ,y, "binary",format(y, '04b'))
+print("nilai:" ,z, "binary",format(z, '07b'))
+
+print("======================")
+z = 4 ^ 11
+print("nilai:" ,x,"binary" ,format(x, '03b'))
+print("nilai:" ,y, "binary",format(y, '04b'))
+print("nilai:" ,z, "binary",format(z, '07b'))
+
+print("======================")
+z = ~4
+print("nilai:" ,x,"binary" ,format(x, '03b'))
+print("nilai:" ,y, "binary",format(y, '04b'))
+print("nilai:" ,z, "binary",format(z, '07b'))
+
+print("======================")
+z = 11 & 4
+print("nilai:" ,x,"binary" ,format(x, '03b'))
+print("nilai:" ,y, "binary",format(y, '04b'))
+print("nilai:" ,z, "binary",format(z, '07b'))
